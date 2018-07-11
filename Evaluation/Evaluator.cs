fd88585b8973b34065e7a5a6dd2be00b7478ec05
Charles <char-cou@hotmail.fr>
2018-07-10 22:18:28 -0400

    Affinage pour la nuit

--- Evaluation/Evaluator.cs
+++ Evaluation/Evaluator.cs
@@ -508,7 +508,7 @@ namespace Evaluation
             //Compute all results and put them in metaResults
             List<Results> listResults = new List<Results>();
 
-            for(double i = 0.03; i < 0.10; i += pas)
+            for(double i = 0.00; i < 0.17; i += pas)
             {
                 Results currentRes = Evaluate(PredictionData, RealData, tuple, i);
                 listResults.Add(currentRes);
